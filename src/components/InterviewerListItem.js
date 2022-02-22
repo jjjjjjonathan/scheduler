5358b137a9169463cf2170c1bc37fbca68508242
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-21 21:33:09 -0500

    Implement stories for InterviewerListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "components/InterviewerListItem.scss";
+import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
+  const { id, name, avatar, selected } = props;
+  const [interviewer, setInterviewer] = useState("");
+  const showName = selected => {
+    if (!selected) {
+      console.log("false");
+      return '';
+    } else {
+      console.log("true");
+      return { interviewer };
+    }
+  };
+  const classes = classNames('intervierwers__item', { 'interviewers__item--selected': selected });
   return (
-    <li className="interviewers__item">
+    <li key={id} className={classes} onClick={() => setInterviewer(id)}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={avatar}
+        alt={showName(selected)}
       />
-      Sylvia Palmer
+      {selected ? `${name}` : ''}
     </li>
   );
 }
