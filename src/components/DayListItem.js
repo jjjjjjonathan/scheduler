c1e5334bdcbc8fb1d8c480d2b5c7baf28a8dd869
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-21 23:09:34 -0500

    Refactor to destructure props

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,7 +3,8 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  const classes = classNames('day-list__item', { 'day-list__item--selected': props.selected, 'day-list__item--full': props.spots === 0 });
+  const { selected, spots, name, setDay } = props;
+  const classes = classNames('day-list__item', { 'day-list__item--selected': selected, 'day-list__item--full': spots === 0 });
   const formatSpots = num => {
     if (num === 0) {
       return 'no spots remaining';
@@ -14,9 +15,9 @@ export default function DayListItem(props) {
     }
   };
   return (
-    <li onClick={() => props.setDay(props.name)} className={classes}>
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    <li onClick={() => setDay(name)} className={classes}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   );
 }
