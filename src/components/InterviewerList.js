2e000d25aff7b725218ded8327342e9603a4617a
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-22 11:25:26 -0500

    Implement InterviewerList with stories working

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+  const { interviewers, interviewer, setInterviewer } = props;
+  const parsedInterviewers = interviewers.map(person => <InterviewerListItem key={person.id} setInterviewer={setInterviewer} selected={person.id === interviewer} {...person} />);
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {parsedInterviewers}
+      </ul>
+    </section>
+  );
+}
