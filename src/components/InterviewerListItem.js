fc0ebb5b441feea676c32b62d1d58897109103ca
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-21 23:07:22 -0500

    Remove unneeded useState

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,9 +3,8 @@ import "components/InterviewerListItem.scss";
 import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
-  const { id, name, avatar, selected } = props;
-  const [interviewer, setInterviewer] = useState("");
-  const classes = classNames('intervierwers__item', { 'interviewers__item--selected': selected });
+  const { id, name, avatar, selected, setInterviewer } = props;
+  const classes = classNames('interviewers__item', { 'interviewers__item--selected': selected });
   return (
     <li key={id} className={classes} onClick={() => setInterviewer(id)}>
       <img
