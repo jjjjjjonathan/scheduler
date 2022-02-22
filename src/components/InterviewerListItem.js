56ca0e6559987e8baa4c6e0105762749b6a5f28b
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-21 21:35:51 -0500

    Remove console logs

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -5,22 +5,13 @@ import classNames from "classnames";
 export default function InterviewerListItem(props) {
   const { id, name, avatar, selected } = props;
   const [interviewer, setInterviewer] = useState("");
-  const showName = selected => {
-    if (!selected) {
-      console.log("false");
-      return '';
-    } else {
-      console.log("true");
-      return { interviewer };
-    }
-  };
   const classes = classNames('intervierwers__item', { 'interviewers__item--selected': selected });
   return (
     <li key={id} className={classes} onClick={() => setInterviewer(id)}>
       <img
         className="interviewers__item-image"
         src={avatar}
-        alt={showName(selected)}
+        alt={name}
       />
       {selected ? `${name}` : ''}
     </li>
