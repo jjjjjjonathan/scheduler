94769258a2bec4a02b649e3607c615ee0a2545a1
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-22 21:20:01 -0500

    Update Appointment index with stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 import "components/Appointment/styles.scss";
 
 const Appointment = props => {
+  const { time, interview } = props;
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={time} />
+      {interview ? <Show {...interview} /> : <Empty />}
+    </article>
   );
 };
 
