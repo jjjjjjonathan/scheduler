42db6a83108a1e2c81ed708fae145e0fc2dfb6dd
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-22 20:41:52 -0500

    Add states to Appointment Form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,36 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
-import { action } from "@storybook/addon-actions/dist/preview";
 
-const Form = props => {
-  const { student, interviewer, interviewers, onSave, onCancel } = props;
+export default (props) => {
+  const { student: studentProp, interviewer: interviewerProp, interviewers, onSave, onCancel } = props;
+  const [student, setStudent] = useState(studentProp || "");
+  const [interviewer, setInterviewer] = useState(interviewerProp || null);
+
+  const reset = () => {
+    setStudent('');
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    onCancel();
+  };
+
+  const submit = event => {
+    event.preventDefault();
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={submit}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
             value={student}
+            onChange={event => setStudent(event.target.value)}
           />
         </form>
         <InterviewerList
           interviewers={interviewers}
           value={interviewer}
-          onChange={action("setInterviewer")}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={onSave}>Save</Button>
         </section>
       </section>
     </main>
   );
 };
-
-export default Form;
