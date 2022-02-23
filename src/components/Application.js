93597917a1f8365de4c44e59c49804f497ad3e6c
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-23 16:00:54 -0500

    Switch from setDay/Days to setState

--- src/components/Application.js
+++ src/components/Application.js
@@ -46,8 +46,17 @@ const appointments = [
 const parsedAppointments = appointments.map(appointment => <Appointment key={appointment.id} {...appointment} />);
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+
+  const setDays = days => {
+    setState(prev => ({ ...prev, days }));
+  };
 
   useEffect(() => {
     axios.get('http://localhost:8001/api/days')
@@ -67,8 +76,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
