b15991586900cfa67ed38ff50eba63f7d12be4f8
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-23 21:43:35 -0500

    Implement useVisualMode hook into appointment index

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,14 +2,25 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
 import "components/Appointment/styles.scss";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 const Appointment = props => {
   const { time, interview } = props;
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview ? <Show {...interview} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && <Show {...interview} />}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => back()} />}
     </article>
   );
 };
