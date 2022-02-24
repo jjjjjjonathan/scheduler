25769e5f43d183c694e29648f9f375cd9012b11a
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-24 16:10:31 -0500

    Remove instances of localhost

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -15,9 +15,9 @@ export default function Application(props) {
 
   useEffect(() => {
     Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
-      axios.get('http://localhost:8001/api/interviewers')
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then(all => {
       const [days, appointments, interviewers] = all;
       setState(prev => ({ ...prev, days: days.data, appointments: appointments.data, interviewers: interviewers.data }));
