6986e0a4ac3ae31ecb30d4a681f8ba2df600b07b
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-23 21:13:00 -0500

    Add custom hooks for mode switching

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const useVisualMode = initial => {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+    setMode(newMode);
+    if (replace) history.pop();
+    setHistory(prev => [...prev, newMode]);
+  };
+
+  const back = () => {
+    if (history.length > 1) history.pop();
+    setMode(history[history.length - 1]);
+  };
+
+  return { mode, transition, back };
+};
+
+export default useVisualMode;
