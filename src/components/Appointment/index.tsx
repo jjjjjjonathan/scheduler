6fa8089b43fcbcc3911dab4bb5efb2696a509149
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-10-17 23:26:01 -0400

    Migrate appointment component to typescript

--- src/components/Appointment/index.tsx
+++ src/components/Appointment/index.tsx
@@ -5,7 +5,7 @@ import Empty from './Empty';
 import Form from './Form';
 import Confirm from './Confirm';
 import Error from './Error';
-import useVisualMode from 'hooks/useVisualMode';
+import useVisualMode from '../../hooks/useVisualMode';
 import 'components/Appointment/styles.scss';
 import Status from './Status';
 
@@ -19,12 +19,37 @@ const EDIT = 'EDIT';
 const ERROR_SAVE = 'ERROR_SAVE';
 const ERROR_DELETE = 'ERROR_DELETE';
 
-export default (props) => {
-  const { time, interview, interviewers, bookInterview, id, deleteInterview } =
-    props;
+type Interviewer = {
+  id: number;
+  name: string;
+  avatar: string;
+};
+
+type Interview = {
+  student: string;
+  interviewer: Interviewer;
+};
+
+type AppointmentProps = {
+  time: string;
+  interview: Interview;
+  interviewers: [Interviewer];
+  bookInterview: Function;
+  id: number;
+  deleteInterview: Function;
+};
+
+export default ({
+  time,
+  interview,
+  interviewers,
+  bookInterview,
+  id,
+  deleteInterview,
+}: AppointmentProps) => {
   const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
 
-  const save = (name, interviewer) => {
+  const save = (name: string, interviewer: Interviewer) => {
     const interview = {
       student: name,
       interviewer,
@@ -32,17 +57,17 @@ export default (props) => {
     transition(SAVING);
     bookInterview(id, interview)
       .then(() => transition(SHOW))
-      .catch((error) => transition(ERROR_SAVE, true));
+      .catch((error: any) => transition(ERROR_SAVE, true));
   };
 
   const deleteAppt = () => transition(CONFIRM);
   const editAppt = () => transition(EDIT);
 
-  const confirmDeleteAppt = (id) => {
+  const confirmDeleteAppt = (id: number) => {
     transition(DELETE, true);
     deleteInterview(id)
       .then(() => transition(EMPTY))
-      .catch((error) => transition(ERROR_DELETE, true));
+      .catch((error: any) => transition(ERROR_DELETE, true));
   };
 
   useEffect(() => {
@@ -55,14 +80,19 @@ export default (props) => {
   }, [interview, mode, transition]);
 
   return (
-    <article className="appointment">
+    <article className='appointment'>
       <Header time={time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && interview && (
         <Show onDelete={deleteAppt} {...interview} id={id} onEdit={editAppt} />
       )}
       {mode === CREATE && (
-        <Form interviewers={interviewers} onCancel={back} onSave={save} />
+        <Form
+          interviewers={interviewers}
+          onCancel={back}
+          onSave={save}
+          student={interview.student ? interview.student : ''}
+        />
       )}
       {mode === SAVING && <Status message={'Saving...'} />}
       {mode === CONFIRM && (
